8d77e1fdc59ac2167249beec1bbe3fe19d39ac10
Tam1SH <ignatternyev54@gmail.com>
2024-03-21 13:41:31 +0300

    first commit

---
+++ web/src/App.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+	<div class="flex-row flex">
+		<div class="rounded-md m-1 w-40 h-10 bg-green-500">
+
+		</div>
+
+		<div class="rounded-md m-1 w-10 h-10 bg-green-500">
+
+		</div>
+		<div class="rounded-md m-1 w-10 h-10 bg-green-500">
+
+		</div>
+		<div class="rounded-md m-1 w-10 h-10 bg-green-500">
+
+		</div>
+		<div class="rounded-md m-1 w-10 h-10 bg-red-500">
+
+		</div>
+	</div>	
+
+
+	<v-btn>
+		Click me
+	</v-btn>
+	<div class="jopa">Hello world</div>
+</template>
+
+<style scoped lang="scss">
+.jopa {
+	@apply border-2;
+}
+</style>
---
+++ web/src/main.ts
@@ -0,0 +1,20 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import './assets/base.css'
+import './assets/main.scss'
+
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+	components,
+	directives,
+})
+  
+createApp(App)
+  .use(router)
+  .use(vuetify)
+  .mount('#app')
